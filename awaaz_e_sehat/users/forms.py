8b770ad527d4475a56105fb2a0a23d62378f23fc
harris-ahmad <24100315@lums.edu.pk>
2023-11-08 02:01:08 +0500

    added docs

--- awaaz_e_sehat/users/forms.py
+++ awaaz_e_sehat/users/forms.py
@@ -1,3 +1,18 @@
+'''
+This file contains the forms for the users app.
+
+CustomUserCreationForm:
+-----------------------
+> This form is used to create a new user.
+> It inherits from the UserCreationForm class of django.contrib.auth.forms
+
+CustomUserChangeForm:
+---------------------
+> This form is used to update the user details.
+> It inherits from the UserChangeForm class of django.contrib.auth.forms
+'''
+
+
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
 
