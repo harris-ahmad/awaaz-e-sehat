dae37900a88db4e903b976a79c6e6f3e400b8393
harris-ahmad <24100315@lums.edu.pk>
2023-12-14 03:06:10 +0500

    formatting import statements

--- app.py
+++ app.py
@@ -1,8 +1,40 @@
-from flask import Flask, render_template, redirect, url_for, flash, session, Blueprint
-from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required, current_user
+from flask import (
+    Flask, 
+    render_template, 
+    redirect, 
+    url_for, 
+    flash, 
+    session, 
+    Blueprint
+)
+
+from flask_login import (
+    LoginManager, 
+    UserMixin, 
+    login_user, 
+    logout_user, 
+    login_required, 
+    current_user
+)
+
 from flask_wtf import FlaskForm
-from wtforms import *
-from wtforms.validators import DataRequired, EqualTo, Length, Optional
+
+from wtforms import (
+    StringField, 
+    PasswordField, 
+    SubmitField, 
+    IntegerField, 
+    SelectField, 
+    DateTimeField
+)
+
+from wtforms.validators import (
+    DataRequired, 
+    EqualTo, 
+    Length, 
+    Optional
+)
+
 import datetime
 import bcrypt
 
