6f618ed0b9801c89efa6e658b050c32655957198
harris-ahmad <24100315@lums.edu.pk>
2023-12-14 03:14:59 +0500

    py file for webforms only

---
+++ webforms.py
@@ -0,0 +1,64 @@
+from flask_wtf import FlaskForm
+
+from wtforms import (
+    StringField, 
+    PasswordField, 
+    SubmitField, 
+    IntegerField, 
+    SelectField, 
+)
+
+from wtforms.validators import (
+    DataRequired, 
+    EqualTo, 
+    Length, 
+    Optional
+)
+
+class LoginForm(FlaskForm):
+    employee_code = StringField('Employee Code', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Log In')
+
+class RegisterForm(FlaskForm):
+    employee_code = StringField('Employee Code', validators=[DataRequired()])
+    full_name = StringField('Full Name', validators=[DataRequired()])
+    password = PasswordField('Password', validators=[DataRequired()])
+    submit = SubmitField('Register')
+
+class ForgotPasswordForm(FlaskForm):
+    employee_code = StringField('Employee Code', validators=[DataRequired()])
+    old_password = PasswordField('Old Password', validators=[DataRequired()])
+    new_password = PasswordField('New Password', validators=[DataRequired(), EqualTo('confirm_password')])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+class ChangePasswordForm(FlaskForm):
+    current_password = PasswordField('Old Password', validators=[DataRequired()])
+    new_password = PasswordField('New Password', validators=[DataRequired(), EqualTo('confirm_password')])
+    confirm_password = PasswordField('Confirm Password', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+class PatientForm(FlaskForm):
+    medical_record_number = StringField('Medical Record Number', validators=[DataRequired()])
+    patient_name = StringField('Patient Name', validators=[DataRequired()])
+    patient_cnic = StringField('Patient CNIC', validators=[Length(min=13, max=13)])
+    patient_age = IntegerField('Patient Age', validators=[DataRequired()])
+    patient_type = SelectField('Patient Type', choices=[('general', 'General'), ('private', 'Private'), \
+                                    ('employee', 'Employee'), ('company_cases', 'Company Cases')], validators=[DataRequired()])
+    weight_kg = IntegerField('Weight (kg)', validators=[Optional()])
+    height_cm = IntegerField('Height (cm)', validators=[Optional()])
+    b_bp_sys = IntegerField('Blood Pressure (Systolic)', validators=[Optional()])
+    b_bp_dia = IntegerField('Blood Pressure (Diastolic)', validators=[Optional()])
+    temperature = IntegerField('Temperature', validators=[Optional()])
+    pulse = IntegerField('Pulse', validators=[Optional()])
+    bsr = IntegerField('BSR', validators=[Optional()])
+    urine_albumin = SelectField('Urine Albumin', choices=[('1+', '1+ (30 mg/dL)'), ('2+', '2+ (100 mg/dL)'), \
+                                    ('3+', '3+ (300 mg/dL)'), ('4+', '4+ (1000 mg/dL)')], validators=[Optional()])
+    hb = IntegerField('HB', validators=[Optional()])
+    spO2 = IntegerField('SpO2', validators=[Optional()])
+    submit = SubmitField('Submit')
+
+class PatientSearchForm(FlaskForm):
+    searched = StringField('Searched', validators=[DataRequired()])
+    submit = SubmitField('Search')
