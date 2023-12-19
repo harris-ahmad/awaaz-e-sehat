993ce1056e2791694b49c3eaf5e6102137fb5486
harris-ahmad <24100315@lums.edu.pk>
2023-12-19 22:01:51 +0500

    basic routes working, doctor session maint. needs to be fixed

--- app.py
+++ app.py
@@ -1,12 +1,10 @@
-from flask import Flask, render_template, redirect, url_for, flash, session, Blueprint
+from flask import Flask, render_template, redirect, url_for, flash, session, Blueprint, request
 from flask_login import LoginManager, login_user, logout_user, login_required
 from flask_wtf import FlaskForm
-from wtforms import *
-from wtforms.validators import DataRequired, EqualTo, Length, Optional
 import datetime
 import bcrypt
 import weasyprint
-import uuid
+import os
 
 from webforms import LoginForm, RegisterForm, PatientSearchForm, UpdateEmployeeProfile, ChangePasswordForm, PatientForm
 
@@ -26,7 +24,7 @@ except Exception as e:
     print(e)
     
 app = Flask(__name__)
-app.config['SECRET_KEY'] = str(uuid.uuid5())
+app.config['SECRET_KEY'] = str(os.urandom(24).hex())
 
 home_blueprint = Blueprint('home', __name__, template_folder='.')
 doctor_blueprint = Blueprint('doctor', __name__, template_folder='doctor')
@@ -38,9 +36,12 @@ app.register_blueprint(home_blueprint, url_prefix='/')
 
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view = 'nurse_login'
+login_manager.blueprint_login_views = {
+    'doctor': '/doctor/login',
+    'nurse': '/nurse/login'
+}
 
-class Nurse():
+class Nurse:
     def __init__(self, employee_code, full_name, password, created_at=datetime.datetime.now()):
         self.employee_code = employee_code
         self.full_name = full_name
@@ -107,7 +108,74 @@ class Nurse():
     def __repr__(self):
         return f'<Nurse {self.employee_code}>'
     
-class Patient():
+class Doctor:
+    def __init__(self, employee_code, full_name, password, created_at=datetime.datetime.now()):
+        self.employee_code = employee_code
+        self.full_name = full_name
+        self.created_at = created_at
+        self.password = password
+
+    @property
+    def verify_password(self, password):
+        return bcrypt.checkpw(password.encode('utf-8'), self.password)
+    
+    def add_doctor(self):
+        db = client['users']
+        collection = db['doctor']
+        existing_doctor = self.check_existing()
+        if existing_doctor is False:
+            new_doctor = dict(
+                employee_code = self.employee_code,
+                full_name = self.full_name,
+                password = bcrypt.hashpw(self.password.encode('utf-8'), bcrypt.gensalt()),
+                created_at = self.created_at
+            )
+            collection.insert_one(new_doctor)
+            return True
+        else:
+            return False
+
+    def check_existing(self):
+        db = client['users']
+        collection = db['doctor']
+        existing_doctor = collection.find_one({'employee_code': self.employee_code})
+        return True if existing_doctor is not None else False
+
+    @staticmethod 
+    def get_doctor(employee_code):
+        db = client['users']
+        collection = db['doctor']
+        doctor = collection.find_one({'employee_code': employee_code})
+        return doctor
+    
+    @staticmethod
+    def is_authenticated():
+        return True
+    
+    @staticmethod
+    def is_active():
+        return True
+    
+    @staticmethod
+    def is_anonymous():
+        return False
+    
+    def get_id(self):
+        return self.employee_code
+    
+    @login_manager.user_loader
+    def load_user(employee_code):
+        db = client['users']
+        collection = db['doctor']
+        user = collection.find_one({'employee_code': employee_code})
+        if not user:
+            return None
+        return Doctor(user['employee_code'], user['full_name'], user['password'], user['created_at'])
+    
+    def __repr__(self):
+        return f'<Doctor {self.employee_code}>'
+    
+class Patient:
     def __init__(self, medical_record_number, full_name, cnic, age, patient_type, weight_kg, \
                  height_cm, b_bp_sys, b_bp_dia, temperature, pulse, bsr, urine_albumin, hb,  \
                  spO2, created_at=datetime.datetime.now(), visit_number=1) -> None:
@@ -185,6 +253,15 @@ def load_user(user_id):
         return None
     return Nurse(user['employee_code'], user['full_name'], user['password'], user['created_at'])
 
+@login_manager.user_loader
+def load_user(user_id):
+    user = Doctor.get_doctor(user_id)
+    if not user:
+        return None
+    return Doctor(user['employee_code'], user['full_name'], user['password'], user['created_at'])
+
+########################################## ERROR HANDLERS ##########################################
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'), 404
@@ -193,10 +270,14 @@ def page_not_found(e):
 def server_error(e):
     return render_template('500.html'), 500
 
+########################################## HOME ##########################################
+
 @app.route('/')
 @app.route('/home')
 def home():
     return render_template('index.html')
+
+########################################## NURSE ##########################################
 
 @app.route('/nurse/register', methods=['GET', 'POST'])
 def nurse_register():
@@ -396,3 +477,161 @@ def nurse_logout():
     session.pop('employee_code', None)
     flash('Logged out successfully!', 'success')
     return redirect(url_for('home'))
+
+########################################## DOCTOR ##########################################
+
+@app.route('/doctor/register', methods=['GET', 'POST'])
+def doctor_register():
+    form = RegisterForm()
+    if form.validate_on_submit():
+        new_doctor = Doctor(
+            employee_code = form.employee_code.data,
+            full_name = form.full_name.data,
+            password = form.password.data,
+        )
+        existing_doctor = new_doctor.check_existing()
+        if not existing_doctor:
+            new_doctor.add_doctor()
+            flash('Registered successfully!', 'success')
+            return redirect(url_for('doctor_login'))
+        else:
+            flash('User already exists!', 'danger')
+            return redirect(url_for('doctor_register'))
+    return render_template('signup.html', form=form)
+
+@app.route('/doctor/login', methods=['GET', 'POST'])
+def doctor_login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        employee_code = form.employee_code.data
+        password = form.password.data
+        doctor = Doctor.get_doctor(employee_code)
+        if doctor and bcrypt.checkpw(password.encode('utf-8'), doctor['password']):
+            login_user(Doctor(
+                employee_code = doctor['employee_code'],
+                full_name = doctor['full_name'],
+                password = doctor['password'],
+                created_at = doctor['created_at']
+            ))
+            session['employee_code'] = employee_code
+            flash('Logged in successfully!', 'success')
+            return redirect(url_for('doctor_dashboard'))
+        else:
+            flash('Invalid credentials!', 'danger')
+            return redirect(url_for('doctor_login'))
+    return render_template('login.html', form=form)
+
+@app.route('/doctor/dashboard')
+@login_required
+def doctor_dashboard():
+    if 'employee_code' in session:
+        employee_code = session['employee_code']
+        first_name = Doctor.get_doctor(employee_code)['full_name'].split()[0].capitalize()
+        curr_date, curr_time = datetime.datetime.now().strftime("%d %B %Y"), datetime.datetime.now().strftime("%H:%M")
+        form = PatientSearchForm()
+        return render_template(
+            template_name_or_list='dashboard.html',
+            first_name=first_name,
+            curr_date=curr_date,
+            curr_time=curr_time,
+            form=form
+        )
+    return redirect(url_for('doctor_login'))
+
+@app.route('/doctor/profile/update', methods=['GET', 'POST'])
+@login_required
+def doctor_update_profile():
+    form = UpdateEmployeeProfile()
+    profile = Doctor.get_doctor(session['employee_code'])
+    curr_full_name, curr_employee_code = profile['full_name'], profile['employee_code']
+    if form.validate_on_submit():
+        full_name = form.full_name.data
+        employee_code = form.employee_code.data
+        if full_name != curr_full_name or employee_code != curr_employee_code:
+            db = client['users']
+            collection = db['doctor']
+            collection.update_one(
+                {'employee_code': curr_employee_code},
+                {'$set': {
+                    'full_name': full_name,
+                    'employee_code': employee_code
+                }}
+            )
+            flash('Profile updated successfully!', 'success')
+            return redirect(url_for('doctor_dashboard'))
+        else:
+            flash('No changes made!', 'danger')
+            return redirect(url_for('doctor_dashboard'))
+    return render_template(
+        template_name_or_list='update-profile.html',
+        form=form,
+        curr_full_name=curr_full_name,
+        curr_employee_code=curr_employee_code
+    )
+
+@app.route('/doctor/change-password', methods=['GET', 'POST'])
+@login_required
+def doctor_change_password():
+    form = ChangePasswordForm()
+    if form.validate_on_submit():
+        current_password = form.current_password.data
+        new_password = form.new_password.data
+        confirm_password = form.confirm_password.data
+        doctor = Doctor.get_doctor(session['employee_code'])
+        if bcrypt.checkpw(current_password.encode('utf-8'), doctor['password']):
+            if new_password == confirm_password:
+                db = client['users']
+                collection = db['doctor']
+                collection.update_one(
+                    {'employee_code': session['employee_code']},
+                    {'$set': {
+                        'password': bcrypt.hashpw(new_password.encode('utf-8'), bcrypt.gensalt())
+                    }}
+                )
+                flash('Password changed successfully!', 'success')
+                return redirect(url_for('doctor_dashboard'))
+            else:
+                flash('Passwords do not match!', 'danger')
+                return redirect(url_for('doctor_change_password'))
+        else:
+            flash('Invalid password!', 'danger')
+            return redirect(url_for('doctor_change_password'))
+    return render_template('change-password.html', form=form)
+
+@app.route('/doctor/search', methods=['POST', 'GET'])
+@login_required
+def doctor_search():
+    form = PatientSearchForm()
+    if form.validate_on_submit():
+        searched = form.searched.data
+        db = client['users']
+        collection = db['patients']
+        patients = collection.find({'$or': [
+            {'full_name': {'$regex': searched, '$options': 'i'}},
+            {'medical_record_number': {'$regex': searched, '$options': 'i'}},
+        ]})
+        return redirect(url_for('doctor_patient', medical_record_number=patients[0]['medical_record_number']))
+    return redirect(url_for('doctor_dashboard'))
+
+
+@app.route('/doctor/logout')
+@login_required
+def doctor_logout():
+    logout_user()
+    session.pop('employee_code', None)
+    flash('Logged out successfully!', 'success')
+    return redirect(url_for('home'))
+
+@app.route('/doctor/vitals', methods=['GET', 'POST']) # temp: to be removed later
+@login_required
+def doctor_vitals():
+    pass
+
+@app.route('/doctor/patient/<string:medical_record_number>')
+@login_required
+def doctor_patient(medical_record_number):
+    patient = Patient.get_patient(medical_record_number)
+    date, time = patient['created_at'].strftime("%d %B %Y"), patient['created_at'].strftime("%H:%M")
+    mr_num = patient['medical_record_number']
+    nurse_name = patient['recorded_by'].split()[0].capitalize()
+    return render_template('patient.html', patient=patient, date=date, time=time, mr_num=mr_num, nurse_name=nurse_name)
