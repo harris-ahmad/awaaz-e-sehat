209d987cf63d09d9bade3ae780e738b1813202bd
harris-ahmad <24100315@lums.edu.pk>
2023-12-29 02:23:15 +0500

    source for database abstracted

---
+++ database.py
@@ -0,0 +1,12 @@
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+uri = "mongodb+srv://harris:harris123@atlascluster.1jhzzr5.mongodb.net/?retryWrites=true&w=majority"
+
+client = MongoClient(uri, server_api=ServerApi('1'))
+
+try:
+    client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+except Exception as e:
+    print(e)
